26107a755c5a54b099d200d28f5db7dff3ae1d8f
Nickinho01 <kaio.rodrigues9695@gmail.com>
2022-09-06 18:49:41 -0300

    Correção de Bug

--- WeedValley/Assets/Script/MovimentPlayer.cs
+++ WeedValley/Assets/Script/MovimentPlayer.cs
@@ -2,19 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class MovimentPlayer : MonoBehaviour
 {
-    public float speed;
+    float input_x = 0;
+    float input_y = 0;
+    float speed = 2.5f;
+
+    Rigidbody2D rb2D;
+    Vector2 movement = Vector2.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
-        transform.position += movement * speed * Time.deltaTime;
+        input_x = Input.GetAxisRaw("Horizontal");
+        input_y = Input.GetAxisRaw("Vertical");
+        movement = new Vector2(input_x, input_y);
+    }
+
+    private void FixedUpdate() 
+    {
+        rb2D.MovePosition(rb2D.position + movement * speed * Time.deltaTime);
     }
 }
