9347471cc9ce17f7de0a04f05c65b699e808c575
Nickinho01 <kaio.rodrigues9695@gmail.com>
2022-09-06 02:38:48 -0300

    Script Moviment

---
+++ WeedValley/Assets/Script/MovimentPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentPlayer : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        transform.position += movement * speed * Time.deltaTime;
+    }
+}
